47dbde5af26d602a43a48816a134fb3be1c6d581
Kajal-11 <kajal11122000@gmail.com>
2020-08-16 20:09:25 +0530

    Message view created, error: using free twilioaccount

--- broadcast/views.py
+++ broadcast/views.py
@@ -2,15 +2,24 @@ from django.shortcuts import render
 from django.conf import settings                                                                                                                                                       
 from django.http import HttpResponse
 from twilio.rest import Client
-
+from user.models import User
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
 
 def broadcast_sms(request):
-    message_to_broadcast = ("Have you played the incredible TwilioQuest "
-                                                "yet? Grab it here: https://www.twilio.com/quest")
+    # # print()
+    # print(str(request) + "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%55")
+    # parse = urlparse.urlparse(request)
+    # print(parse_qs(parsed.query)['id'] + "%%%%%%%%%%%%%%%%%%&&&&&&&&&")
+    # # print()
+    # print(request.GET.get('id')+ "%%%%%%%%%%%%%%%%%%&&&&&&&&&")
+    id = request.GET.get('id')
+    recipient = User.objects.filter(id=id).first()
+    message_to_broadcast = ("Dear " + recipient.member.name + ",\nYour registration with Panha Foundation has been completed successfully!")
     client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-    for recipient in settings.SMS_BROADCAST_TO_NUMBERS:
-        if recipient:
-            client.messages.create(to=recipient,
-                                   from_=settings.TWILIO_NUMBER,
-                                   body=message_to_broadcast)
-    return HttpResponse("messages sent!", 200)
+    if recipient:
+        cm = str(recipient.contact)
+       	client.messages.create(to=cm,
+                               from_=settings.TWILIO_NUMBER,
+                               body=message_to_broadcast)
+    return redirect(login)
