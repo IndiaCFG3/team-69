c0ec9a4dbc4e95493a855c1e7828e788ec657256
Meet Shah <meet8june@gmail.com>
2020-08-16 16:31:58 +0530

    dashboard url and view

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home, dashboard
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('dashboard/', dashboard, name='dashboard')
 ]
