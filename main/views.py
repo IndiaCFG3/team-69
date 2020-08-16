3b4b58c61bc7ecd9cd91f724b44620be4a4a4aab
Meet Shah <meet8june@gmail.com>
2020-08-16 20:07:30 +0530

    minor bug fix

--- main/views.py
+++ main/views.py
@@ -8,10 +8,12 @@ def index(request):
     }
     return render(request, 'main/index.html', context)
 
+
 def contact(request):
     context = {
     }
     return render(request, 'main/contact.html', context)
+
 
 def home(request):
     schemes = Schemes.objects.all()
@@ -25,7 +27,7 @@ def dashboard(request):
     if(request.user.role == "end_user"):
         return render(request, 'main/dashboard.html')
     elif(request.user.role == "volunteer"):
-        volunteer_member = VolunteerMember.objects.filter(
-            volunteer=request.user.volunteer).all()
-        print(volunteer_member)
-        return render(request, 'user/dashboard.html')
+        # volunteer_member = VolunteerMember.objects.filter(
+        #     volunteer=request.user.volunteer).all()
+        # print(volunteer_member)
+        return render(request, 'user/vol_dashboard.html')
