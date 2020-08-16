53e17c35601b2f9d02ce66c2ec65667ff8b7d525
Kajal-11 <kajal11122000@gmail.com>
2020-08-16 20:46:24 +0530

    applied schemes

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from schemes.models import Schemes
 from volunteers.models import VolunteerMember
+from member.models import Member
 
 def index(request):
     context = {
@@ -29,8 +30,11 @@ def dashboard(request):
         # volunteer_member = VolunteerMember.objects.filter(
         #     volunteer=request.user.volunteer).all()
         # print(volunteer_member)
-<<<<<<< HEAD
-        return render(request, 'user/dashboard.html')
-=======
         return render(request, 'user/vol_dashboard.html')
->>>>>>> 6b2d3c9040c8cf05b3801be96245d18782c9ff5c
+
+def appliedSchemes(request):
+    context = {
+        scheme: Schemes.objects.filter(applied_schemes=request.user.member),
+    }
+    return render(request, '')
+
--- schemes/urls.py
+++ schemes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import searchSchemePage, searchResultsView
+from .views import searchSchemePage, searchResultsView, appliedSchemes
 
 urlpatterns = [    
     path('schemes/', searchSchemePage, name='schemes'),
-    path('schemes/search', searchResultsView, name='schemes'),
+    path('schemes/search/', searchResultsView, name='schemes'),
+    path('applied-schemes/', appliedSchemes, name='applied-schemes'),
 ]
